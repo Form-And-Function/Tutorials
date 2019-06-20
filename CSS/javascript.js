0673df562ce72bb0cbda969de874ca98a657dcad
StudentSydney <sydv793@gmail.com>
2019-06-19 19:54:04 -0700

    a...

--- CSS/javascript.js
+++ CSS/javascript.js
@@ -1,12 +1,18 @@
 var myTimeline = anime.timeline(parameters);
 var obs = [];
-var slides = [];
+var slides = [
+    [],
+    []
+];
 
 class slide {
-    constructor(...obs){
+    constructor(duration, idx, ...obs){
+        this.duration = duration;
         this.obs = [];
+        this.idx = idx;
+        this.prevIdx = idx - 1;
+        this.nextIdx = idx + 1;
         this.addObs(obs);
-        slides.push(this);
 
     }
     addObs(...obs){
@@ -20,9 +26,20 @@ class slide {
             svg = $('<object type="image/svg+xml" data="'+ob+'">');
         }
         else {
-            svg = ob;
+            svg = $(ob);
         }
         this.obs.push(svg);
     }
-}
+    next(){
+        slides[nextIdx].play();
+    }
+    play(){
 
+    }
+    back(){
+        this.obs.forEach( ob =>
+            body.remove(ob)
+        )
+        slides[prevIdx].play();
+    }
+}
