fed6fa20bdf4dbadfa98b8406a277e9ace0180d9
StudentSydney <sydv793@gmail.com>
2019-06-19 18:46:04 -0700

    svg flexibility

--- CSS/javascript.js
+++ CSS/javascript.js
@@ -2,3 +2,23 @@ var myTimeline = anime.timeline(parameters);
 var obs = [];
 var slides = [];
 
+class slide {
+    constructor(){
+        this.obs = [];
+    }
+    addObs(...obs){
+        obs.forEach((ob)=>
+            this.addOb(ob)
+        );
+    }
+    addOb(ob){
+        var svg;
+        if(typeof ob === "text"){
+            svg = $('<object type="image/svg+xml" data="'+ob+'">');
+        }
+        else {
+            svg = ob;
+        }
+        this.obs.push(svg);
+    }
+}
