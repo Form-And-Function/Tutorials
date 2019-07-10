619815c5ce455de0afe316db96234018d3d89bda
StudentSydney <sydv793@gmail.com>
2019-07-09 19:01:31 -0700

    dropdowns

--- CSS_Lesson/javascript.js
+++ CSS_Lesson/javascript.js
@@ -1,4 +1,4 @@
-var draw = SVG('drawing')
+var d = Snap('body');
 var myTimeline = anime.timeline(parameters);
 var obs = [];
 var slides = [
@@ -51,7 +51,7 @@ SVG.extend(SVG.Shape, {
         this.node.classList.push(className);
         return this;
     }
-})
+});
 
 function popIn($elem, width, delay = 0) {
     var scale = width/$elem.width();
@@ -87,6 +87,7 @@ titleSlide.play = () => {
         var size = sizes[i];
         popIn(this, size), i*100;
     });
+    
 }
 
 titleSlide.obscircles = [
@@ -118,4 +119,132 @@ function dropdown(...strs){
     }
     )
     return $wrap;
+}
+
+
+
+var selectionLv1 = function(){
+    const colors = ['pink', 'red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+const sizes = ['10px', '100px', '500px', '1in', '1cm'];
+var propertyValuesPairs = {
+	background: colors,
+  width: sizes,
+  height: sizes,
+  color: colors
+};
+
+function updateCSS(target, property, value) {
+  let styleStr = `${target}{${property}: ${value};}`;
+  let $styleTag = $('<style>').addClass('tempSheet').html(styleStr);
+  $('head').append($styleTag);
+}
+
+function addOptions($dropDown, options) {
+	$dropDown.data('selected', false)
+  .remove('.options');
+  deselect($dropDown);
+  for (let [idx, opt] of options.entries()) {
+
+    let $fill = $dropDown.children('.fill');
+    $("<button>")
+      .addClass("option")
+      .text(opt)
+      .css('margin-top', idx * 5 + "vh")
+      .click(() => {
+
+        let $this = $(this);
+
+
+        if ($this.data('clicked')) {
+        $opt.data('clicked', false)
+            .css('border-width', '0');
+          deselect($dropDown);
+        } else {
+          $dropDown.children('.option').css('border-width', '3%').data('clicked', false);
+          $this.data('clicked', true)
+            .css('border-width', '0');
+          $fill.text(opt)
+            .css('transform', 'scaleY(1)')
+        }
+        $dropDown.data('clicked', true);
+
+      })
+      .appendTo($dropDown);
+  }
+}
+function deselect($dropDown){
+	
+          $dropDown.children('.fill').text('')
+            .css('transform', '');
+          $dropDown.data('clicked', false);
+}
+function makeDropDown(options) {
+  var $container = $("<div>")
+    .addClass("dropDown");
+
+  var $fill = $("<div>").addClass("fill")
+    .appendTo($container);
+  addOptions($container, options);
+
+  $container.hover(
+    () => {
+      $container.children(".option").show().each((i, elem) => {
+        let e = $(elem);
+        setTimeout(() => {
+          e.css('transform', 'scaleY(1)');
+        }, 500 * i);
+
+      });
+    },
+    () => {
+
+      $($container.children(".option").get().reverse()).each((i, elem) => {
+        let e = $(elem);
+        setTimeout(() => {
+
+          e.css('transform', 'scaleY(0)');
+          setTimeout(() => {
+            e.hide()
+          }, 500);
+
+        }, 500 * i);
+
+      });
+    }
+
+  );
+  return $container;
+}
+const $valueSelector = makeDropDown(propertyValuesPairs.values()[0])
+  .children('.option');
+
+const $propertySelector = makeDropDown(propertyValuesPairs.keys())
+  .children('.option').click(() => {
+    let values = propertyValuesPairs[property];
+    addOptions($valueSelector, values);
+  });
+
+$('body').append(
+  $('<div>').addClass('flexWrap')
+  .append(
+    $('<div>').addClass('topHalf').append(
+      $('<span>').text('Give the'),
+      $valueSelector,
+      $('<span>').text('a fancy'),
+      $propertySelector,
+      $('<span>').text("that's"),
+      makeDropDown(["background", "width", "height", "color"]),
+      $('<span>').text(".")
+    ),
+    $('<div>').addClass('bottomHalf').append(
+
+      $('<div>').append(
+        $('<button>').text('Click me!'),
+        $('<p>').html('This is a paragraph <strong>inside</strong> a div.')
+      ),
+      $('<p>').html('This is a paragraph <em>outside</em> a div.')
+    )
+  )
+);
+
 }
